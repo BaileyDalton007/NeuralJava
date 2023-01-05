11e98a2a31c7f491cf935f22178fc841c4030ae8
Bailey ds <baileydalton007@gmail.com>
2023-01-05 17:10:43 -0500

    Adds exception for creating too small of a network

---
+++ src/NetworkTooSmallException.java
@@ -0,0 +1,13 @@
+/**
+ * Exception for when a Network is initialized with too few layers.
+ * 
+ * @author Bailey Dalton
+ */
+public class NetworkTooSmallException extends Exception {
+    /**
+     * Constructor for a NetworkTooSmallException.
+     */
+    public NetworkTooSmallException() {
+        super("Network is too small to be created. The minimum amount of layers a network can have is 2, try adding more.");
+    }
+}
