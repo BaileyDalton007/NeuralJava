58057d68970adb976a28f846cd749f7d76cd1e48
Bailey ds <baileydalton007@gmail.com>
2023-01-05 17:47:57 -0500

    Adds constructor for Dense Neural Networks

---
+++ src/DenseNeuralNetwork.java
@@ -0,0 +1,39 @@
+/**
+ * A class for Dense Neural Networks where every neuron in a layer is connected
+ * to every neuron in the previous layer.
+ * 
+ * @author Bailey Dalton
+ */
+public class DenseNeuralNetwork {
+    // Array for storing the layer objects in the network.
+    public Layer[] layerArray;
+
+    // Array for storing the weight matrix objects connecting each layer to the
+    // previous in the network.
+    public WeightMatrix[] layerWeights;
+
+    /**
+     * Constructor for a dense neural network object.
+     * 
+     * @param layerArray An array of layers that will make up the network
+     * @throws NetworkTooSmallException Will be thrown if a network is initialized
+     *                                  with less than 2 layers
+     */
+    public DenseNeuralNetwork(Layer[] layerArray) throws NetworkTooSmallException {
+        // Throw an exception if a network is created with less than two layers.
+        if (layerArray.length < 2)
+            throw new NetworkTooSmallException();
+
+        this.layerArray = layerArray;
+
+        // Weight matrix array will be the same size as the amount of layers minus one
+        // as the input layer layer will not have a weight matrix.
+        this.layerWeights = new WeightMatrix[layerArray.length - 1];
+
+        // Iterates through each set of weights (between two layers) and initializes an
+        // appropriately sized weight matrix.
+        for (int i = 0; i < layerWeights.length; i++) {
+            layerWeights[i] = new WeightMatrix(layerArray[i + 1].size(), layerArray[i].size());
+        }
+    }
+}
