398e632311662f8dd5248464e5ca6f42ad648aa9
Bailey ds <baileydalton007@gmail.com>
2023-01-06 17:14:13 -0500

    Starts testing

--- src/test/java/com/github/baileydalton007/TestDenseNeuralNetwork.java
+++ src/test/java/com/github/baileydalton007/TestDenseNeuralNetwork.java
@@ -1,5 +1,25 @@
 package com.github.baileydalton007;
 
+import org.junit.jupiter.api.Test;
+
+import com.github.baileydalton007.models.DenseNeuralNetwork;
+import com.github.baileydalton007.models.components.Layer;
+import com.github.baileydalton007.activationfunctions.*;;
+
+/**
+ * Unit test for testing the dense neural network model.
+ * 
+ * @author Bailey Dalton
+ */
 public class TestDenseNeuralNetwork {
 
+    @Test
+    public void test() {
+        DenseNeuralNetwork model = new DenseNeuralNetwork(new Layer[] {
+                new Layer(3, new ReLUFunction()),
+                new Layer(2, new ReLUFunction()),
+                new Layer(1, new SigmoidFunction()),
+        });
+
+    }
 }
