e3266b18a11ebe470b5973a3a44e600e3fb786e1
Bailey ds <baileydalton007@gmail.com>
2023-01-06 21:10:48 -0500

    Adds forward propagation testing for dense neural networks

--- src/test/java/com/github/baileydalton007/models/TestDenseNeuralNetwork.java
+++ src/test/java/com/github/baileydalton007/models/TestDenseNeuralNetwork.java
@@ -1,23 +1,44 @@
 package com.github.baileydalton007.models;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
 import org.junit.jupiter.api.Test;
 
+import com.github.baileydalton007.exceptions.IncompatibleInputException;
 import com.github.baileydalton007.models.components.Layer;
 
 /**
- * Unit test for testing the dense neural network model.
+ * JUnit test for testing the dense neural network model.
  * 
  * @author Bailey Dalton
  */
 public class TestDenseNeuralNetwork {
 
+    /**
+     * Test for the forward propagation algorithm on dense neural networks.
+     */
     @Test
-    public void test() {
-        DenseNeuralNetwork model = new DenseNeuralNetwork(new Layer[] {
+    public void testForwardPropagation() {
+        // Creates a neural network to test.
+        DenseNeuralNetwork m = new DenseNeuralNetwork(new Layer[] {
                 new Layer(3, "relu"),
                 new Layer(2, "relu"),
-                new Layer(1, "sigmoid"),
         });
 
+        // Checks that are all outputs are zero when initialized, because all weights
+        // are zero.
+        assertArrayEquals(new double[] { 0.0, 0.0 }, m.ForwardPropagation(new double[] { 1.0, 2.0, 3.0 }));
+
+        // Tests that an exception is thrown if input array and the input layer are not
+        // the same size.
+        try {
+            m.ForwardPropagation(new double[] { 1.0, 2.0 });
+            fail();
+        } catch (IncompatibleInputException e) {
+            // Expected output, nothing happens.
+        } catch (Exception e) {
+            fail();
+        }
     }
 }
