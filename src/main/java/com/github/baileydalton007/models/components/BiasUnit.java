d4290dfb4df9d5629050b6e356e730d555d459a7
Bailey ds <baileydalton007@gmail.com>
2023-01-07 21:42:25 -0500

    Adds bias unit class

---
+++ src/main/java/com/github/baileydalton007/models/components/BiasUnit.java
@@ -0,0 +1,36 @@
+package com.github.baileydalton007.models.components;
+
+/**
+ * Class for bias units for neural networks.
+ * 
+ * @author Bailey Dalton
+ */
+public class BiasUnit {
+    // Stores the value of a bias unit.
+    private double value;
+
+    /**
+     * Constructor for a bias unit, initializes its value to 1.0;
+     */
+    public BiasUnit() {
+        value = 1.0;
+    }
+
+    /**
+     * Getter for the bias unit's value.
+     * 
+     * @return The unit's value
+     */
+    public double getValue() {
+        return value;
+    }
+
+    /**
+     * Setter for the bias unit's value.
+     * 
+     * @param value The new value stored in the bias unit
+     */
+    public void setValue(double value) {
+        this.value = value;
+    }
+}
